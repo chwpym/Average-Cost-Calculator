5538ff38d96b8ccbee7be09610aa83652d3fab8e
Julio Cezar Bicudo Barbosa <chupym.julio@gmail.com>
2025-08-16 14:46:45 +0000

    pelo que vi as calculadoras estão todas no mesmo arquivo, tem como separ

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,20 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const formatCurrency = (value: number) => {
+  if (isNaN(value)) return "R$ 0,00";
+  return new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  }).format(value);
+};
+
+export const formatNumber = (value: number, minimumFractionDigits = 0, suffix = "") => {
+    if (isNaN(value)) return `0${suffix}`;
+    const formatted = new Intl.NumberFormat("pt-BR", {
+      minimumFractionDigits: minimumFractionDigits,
+      maximumFractionDigits: 2,
+    }).format(value);
+    return `${formatted}${suffix}`;
+};
